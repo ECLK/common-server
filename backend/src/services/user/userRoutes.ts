422fe5d18b6be6654386af2ee26bb0c8d5e0aa71
mrclemrkz <awesomeisme007@gmail.com>
2019-07-03 11:43:11 +0530

    get user by id.

--- backend/src/services/user/userRoutes.ts
+++ backend/src/services/user/userRoutes.ts
@@ -1,7 +1,17 @@
 import { Request, Response } from "express";
-import { getUsers, addUser } from "./userService";
+import { getUsers, addUser, getUserById } from "./userService";
 
 export default [
+    {
+        path: "/api/v1/users",
+        method: "post",
+        handler: [
+            async (req: Request, res: Response) => {
+                const result = await addUser(req.body);
+                res.status(201).send(result);
+            }
+        ]
+    },
     {
         path: "/api/v1/users",
         method: "get",
@@ -13,11 +23,11 @@ export default [
         ]
     },
     {
-        path: "/api/v1/users",
-        method: "post",
+        path: "/api/v1/users/:id",
+        method: "get",
         handler: [
             async (req: Request, res: Response) => {
-                const result = await addUser(req.body);
+                const result = await getUserById(req);
                 res.status(200).send(result);
             }
         ]
--- backend/src/services/user/userService.ts
+++ backend/src/services/user/userService.ts
@@ -1,3 +1,4 @@
+import { Request } from "express";
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../../repository/UserRepository";
 import { User } from "../../entity/User";
@@ -11,5 +12,12 @@ export const getUsers = async () => {
 export const addUser = async (user: User) => {
     const userRepository = getCustomRepository(UserRepository);
     const result = userRepository.save(user);
+    return user;
+}
+
+export const getUserById = async (req: Request) => {
+    const userRepository = getCustomRepository(UserRepository);
+    const userId: number = req.params.id;
+    const user = userRepository.findOne({ id: userId});
     return user;
 }
